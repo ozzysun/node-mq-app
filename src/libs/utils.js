d1507ab340623a1012b1474cb0ef263ba6f17dce
ozzysun <ozzysun@gmail.com>
2020-07-26 09:41:27 +0800

    add yargs

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -1,6 +1,7 @@
 const yaml = require('js-yaml')
 const path = require('path')
 const fs = require('fs')
+const { argv } = require('yargs')
 // yaml 讀取
 const readYAML = async(sourceFile) => {
   const filePath = path.resolve(sourceFile)
@@ -13,4 +14,11 @@ const readYAML = async(sourceFile) => {
     }
   })
 }
-module.exports = { readYAML }
+const getArgs = () => {
+  const hostId = argv.host || argv.h || null
+  const channelId = argv.cannel || argv.c || null
+  const queueId = argv.queue || argv.q || null
+  const result = { hostId, channelId, queueId }
+  return result
+}
+module.exports = { readYAML, getArgs }
